66f35a5ba4a0cf691121275566211bfc78d0b7cb
Dany_Gold1 <periodista9417yt@gmail.com>
2026-06-17 17:56:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-const PLACES = [
-    105267492031116,
-    98765432101234,
-    12345678901234
-];
 
-// URL de tu Worker
-const WORKER_URL = "https://obtenerdatoscloud.periodista9417yt.workers.dev/";
-
-// Datos opcionales para launchData
-const DEEP_LINK_DATA = {
-    roomId: 2
-};
