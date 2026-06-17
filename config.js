792629ace68da1d4ae3f171d6e1ae395bf0eb6c8
Dany_Gold1 <periodista9417yt@gmail.com>
2026-06-17 16:00:21 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const PLACES = [
+    105267492031116,
+    98765432101234,
+    12345678901234
+];
+
+// URL de tu Worker
+const WORKER_URL = "https://tu-worker.tudominio.workers.dev";
+
+// Datos opcionales para launchData
+const DEEP_LINK_DATA = {
+    roomId: 2
+};
